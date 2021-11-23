a1af7d007e6484e42551c4c8d3da2676560d3f7c
drishyab5737 <94509562+drishyab5737@users.noreply.github.com>
2021-11-23 15:37:53 +0530

    Add files via upload

---
+++ CountVowelsConsonants.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Drishya
+ */
+public class CountVowelsConsonants {    
+    public static void main(String[] args) {    
+            
+        //Counter variable to store the count of vowels and consonant    
+        int vCount = 0, cCount = 0;    
+            
+        //Declare a string    
+        String str = "drishya";    
+            
+        //Converting entire string to lower case to reduce the comparisons    
+        str = str.toLowerCase();    
+            
+        for(int i = 0; i < str.length(); i++) {    
+            //Checks whether a character is a vowel    
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {    
+                //Increments the vowel counter    
+                vCount++;    
+            }    
+            //Checks whether a character is a consonant    
+            else if(str.charAt(i) >= 'a' && str.charAt(i)<='z') {      
+                //Increments the consonant counter    
+                cCount++;    
+            }    
+        }    
+        System.out.println("Number of vowels: " + vCount);    
+        System.out.println("Number of consonants: " + cCount);    
+    }    
+}   
